90681379a106bc8d695487790ab420e620a9910d
Brachmantya <6182001057@gmail.com>
2024-11-26 11:35:14 +0700

    menambahkan java spring boot

---
+++ PresensiPegawai_kafe-IF/src/main/java/PresensiPegawai_kafe_IF/example/PresensiPegawai_kafe_IF/PresensiPegawaiKafeIfApplication.java
@@ -0,0 +1,13 @@
+package PresensiPegawai_kafe_IF.example.PresensiPegawai_kafe_IF;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PresensiPegawaiKafeIfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PresensiPegawaiKafeIfApplication.class, args);
+	}
+
+}
---
+++ PresensiPegawai_kafe-IF/src/test/java/PresensiPegawai_kafe_IF/example/PresensiPegawai_kafe_IF/PresensiPegawaiKafeIfApplicationTests.java
@@ -0,0 +1,13 @@
+package PresensiPegawai_kafe_IF.example.PresensiPegawai_kafe_IF;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PresensiPegawaiKafeIfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
